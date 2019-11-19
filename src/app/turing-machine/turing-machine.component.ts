b0a64ef19618167ecd2315e6b527113878b68134
EnzoJ0se <enzojose.reus@gmail.com>
2019-11-18 23:29:26 -0300

    Codigo inical para ler a fita

--- src/app/turing-machine/turing-machine.component.ts
+++ src/app/turing-machine/turing-machine.component.ts
@@ -7,9 +7,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TuringMachineComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  actionTable = [
+    {
+      
+      stateSimbol: null,
+      actions: [{
+        
+        currentSimbol: null,
+        nextSateSimbol: null,
+        writeSimbol: null,
+        direction: null,
+        isEndState: 0
+        
+      }]
+      
+    }
+  ];
+  
+  currentState: any;
+  
+  startProcess(tapeSimbols: Array<any>) {
+
+    this.currentState = '->';
+    for (let i = 0; i < tapeSimbols.length; i) {
+      
+       
+      let currentSimbol = tapeSimbols[i];
+      // seta objeto do estado e a ação a ser feita
+      let state = this.getState();
+      let action = this.getAction(state, currentSimbol);
+
+      // se for estado final para a maquina
+      if(action.isEndState) {
+        break;
+
+      } else {
+
+        // atualiza o estado
+        this.currentState = action.nextSateSimbol;
+
+        // escreve o simbolo na fita
+        console.log(action.writeSimbol);
+
+        // verifica a direção e move o cabeçote
+        if(action.direction == 'left') {
+          i--;
+
+        } else if( action.direction == 'right') {
+          i++;
+        }
+
+      }
+
+    }
+
+  }
+
+  getAction(state, simbol) {
+    //retorna ação a ser executada
+    return state.actions.map((action) => {
+        if(action.currentSimbol == simbol) {
+          return action;
+        }
+    })
+  }
+
+  getState() {
+    // retorna objeto do estado
+    return this.actionTable.map( (state) => {
+      if(this.currentState == state.stateSimbol) {
+        return state;
+      }
+    });
   }
 
 }
