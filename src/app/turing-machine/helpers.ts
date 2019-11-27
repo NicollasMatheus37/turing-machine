3fb089c01e9444cf988e00625571f9b269fca8e0
EnzoJ0se <enzojose.reus@gmail.com>
2019-11-27 08:56:43 -0300

    exemplo de subtração

--- src/app/turing-machine/helpers.ts
+++ src/app/turing-machine/helpers.ts
@@ -1,9 +1,9 @@
 export class MachineActions {
-    state: string | number = "";
-    entrySimbol: string = "";
-    nextSate: string | number = "";
-    writeSimbol: string = "";
-    direction: string = "";
+    state: string | number = '';
+    entrySimbol: string = '';
+    nextSate: string | number = '';
+    writeSimbol: string = '';
+    direction: string = '';
 }
 
 export class MultiplicationExample {
@@ -146,44 +146,73 @@ export class MultiplicationExample {
     ];
 }
 
-// export class SubtractionExample {
-//     symbols = [];
-//     states = [];
-//     actions = [
-//         {
-//             state: "",
-//             entrySimbol: "",
-//             nextSate: "",
-//             writeSimbol: "",
-//             direction: "",
-//         },
-//         {
-//             state: "",
-//             entrySimbol: "",
-//             nextSate: "",
-//             writeSimbol: "",
-//             direction: "",
-//         },
-//         {
-//             state: "",
-//             entrySimbol: "",
-//             nextSate: "",
-//             writeSimbol: "",
-//             direction: "",
-//         },
-//         {
-//             state: "",
-//             entrySimbol: "",
-//             nextSate: "",
-//             writeSimbol: "",
-//             direction: "",
-//         },
-//         {
-//             state: "",
-//             entrySimbol: "",
-//             nextSate: "",
-//             writeSimbol: "",
-//             direction: "",
-//         }
-//     ];
-// }
+export class SubtractionExample {
+    // subtração simples ( sem numeros negativos) ps: manter o formato *****_**
+    symbols = ['->', '_', '*'];
+    states = ['->', '0', '1', '2', '3'];
+    actions = [
+        {
+            state: '->',
+            entrySimbol: '->',
+            nextSate: '0',
+            writeSimbol: '->',
+            direction: 'D',
+        },
+        {
+            state: '0',
+            entrySimbol: '*',
+            nextSate: '0',
+            writeSimbol: '*',
+            direction: 'D',
+        },
+        {
+            state: '0',
+            entrySimbol: '_',
+            nextSate: '1',
+            writeSimbol: '_',
+            direction: 'D',
+        },
+        {
+            state: '1',
+            entrySimbol: '*',
+            nextSate: '2',
+            writeSimbol: '*',
+            direction: 'E',
+        },
+        {
+            state: '1',
+            entrySimbol: '_',
+            nextSate: '1',
+            writeSimbol: '_',
+            direction: 'PARA',
+        },
+        {
+            state: '2',
+            entrySimbol: '*',
+            nextSate: '3',
+            writeSimbol: '_',
+            direction: 'D',
+        },
+        {
+            state: '2',
+            entrySimbol: '_',
+            nextSate: '2',
+            writeSimbol: '_',
+            direction: 'E',
+        },
+        {
+            state: '3',
+            entrySimbol: '*',
+            nextSate: '1',
+            writeSimbol: '_',
+            direction: 'D',
+        },
+        {
+            state: '3',
+            entrySimbol: '_',
+            nextSate: '3',
+            writeSimbol: '_',
+            direction: 'D',
+        }
+    ];
+}
